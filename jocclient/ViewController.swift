ea157909147dc1724f21f87233fdda09c7b6b727
bios90 <bios90@mail.ru>
2020-10-10 21:38:52 +0300

    Initial Commit

---
+++ jocclient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  jocclient
+//
+//  Created by Филипп Бесядовский on 10.10.2020.
+//  Copyright © 2020 justordercompany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
