9e419176a9484f9c94b4cadd047359503a034f25
bios90 <bios90@mail.ru>
2021-02-09 02:24:27 +0300

    *** Almost all done ***

---
+++ jocclient/networking/responses/RespOrderSingle.swift
@@ -0,0 +1,9 @@
+//
+//  RespOrderSingle.swift
+//  jocclient
+//
+//  Created by Филипп Бесядовский on 06.02.2021.
+//  Copyright © 2021 justordercompany. All rights reserved.
+//
+
+import Foundation
